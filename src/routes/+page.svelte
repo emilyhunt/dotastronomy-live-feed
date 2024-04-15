4953ada13494ad31a2ee080ce9d6eb07502e52c3
emilyhunt <emily.hunt.physics@gmail.com>
2024-04-15 15:48:34 +0200

    Styling tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,25 @@
 <div class="flex-row-container">
-  <div class="flex-row-item">
+  <div class="flex-row-item" style="border-right: solid;">
     <h2>Schedule:</h2>
-    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR5i_a8bhOM55AVsqXyqDKrUzDHv4mIJ3YEzxXKblQjOEXIEDTCQS3Ml-rIm_mPZcxc6Dd_HIwpPgQ3/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false&amp;wmode=opaque" width="100%" data-embed="true" height="800px"></iframe>    
+    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR5i_a8bhOM55AVsqXyqDKrUzDHv4mIJ3YEzxXKblQjOEXIEDTCQS3Ml-rIm_mPZcxc6Dd_HIwpPgQ3/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false&amp;wmode=opaque" width="100%" height="850px" data-embed="true"></iframe>    
   </div>
 
-  <div class="flex-row-item" style="max-width: 25vw">
-    <div style="margin-top: 50%; border: solid; border-radius: 30px; padding: 10px;">
+  <div class="flex-row-item" style="max-width: 25vw;">
+    <div  class="notice" style="margin-top: 50%; ">
       <h1>Welcome to .Astronomy 13!</h1>
+      <p>ESAC, Madrid | April 2024</p>
+    </div>
+
+    <div  class="notice">
+      <h2>Slack</h2>
       <p>Please sign up to the Slack!<br>(see emails for invite link)</p>
     </div>
   </div>
 
-  <div class="flex-row-item" style="overflow-y: scroll">
+  <div class="flex-row-item" style="overflow-y: scroll; border-left: solid;">
     <h2>Bluesky posts with #dotastro</h2>
     <bsky-embed
-      search="#astronomy"
+      search="#dotastro"
       mode="dark"
       limit="20"
       link-target="_blank"
@@ -34,7 +39,7 @@
     text-align: center;
   }
   h2 {
-    font-size: 2em;
+    font-size: 2.5em;
     text-align: center;
   }
 
@@ -49,7 +54,7 @@
   }
 
   .flex-row-container > .flex-row-item {
-    flex: 1 0 30%; /*grow | shrink | basis */
+    flex: 1 1 50%; /*grow | shrink | basis */
     height: 100vh;
   }
 
@@ -57,7 +62,11 @@
     border: 20px;
     /* background-color: white; */
     border-color: white;
-    margin-left: 40px;
-    margin-right: 80px;
+    padding-left: 80px;
+    padding-right: 80px;
+  }
+
+  .notice {
+    border: solid; border-radius: 30px; padding: 10px; margin-top: 100px;
   }
 </style>
